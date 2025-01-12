f29f9acc3e2fcba8d9c9f6cfca3113a640f59a6c
amtins <andre.martins@rts.ch>
2025-01-12 20:22:44 +0100

    style(svg-sprite-config): use quotes instead double quotes

--- src/svg-sprite.config.js
+++ src/svg-sprite.config.js
@@ -1,4 +1,4 @@
-import url from "url";
+import url from 'url';
 import path from 'path';
 
 const fileName = url.fileURLToPath(import.meta.url);
@@ -6,27 +6,27 @@ const dirName = path.dirname(fileName);
 const baseDirName = path.dirname(dirName);
 
 export default {
-  "dest": ".",
-  "mode": {
-    "symbol": {
-      "inline": true,
-      "sprite": "vjs-sprite-icons.svg",
-      "dest": "vjs-sprite",
-      "example": {
-        "dest": "index.html",
-        "template": path.join(baseDirName, "template/sprite.html")
+  'dest': '.',
+  'mode': {
+    'symbol': {
+      'inline': true,
+      'sprite': 'vjs-sprite-icons.svg',
+      'dest': 'vjs-sprite',
+      'example': {
+        'dest': 'index.html',
+        'template': path.join(baseDirName, 'template/sprite.html')
       }
     }
   },
-  "shape": {
-    "id": { "generator": "vjs-icon-%s" },
-    "dimension": {
-      "maxWidth": 48,
-      "maxHeight": 48
+  'shape': {
+    'id': { 'generator': 'vjs-icon-%s' },
+    'dimension': {
+      'maxWidth': 48,
+      'maxHeight': 48
     }
   },
-  "svg": {
-    "xmlDeclaration": false,
-    "doctypeDeclaration": false
+  'svg': {
+    'xmlDeclaration': false,
+    'doctypeDeclaration': false
   }
 };
